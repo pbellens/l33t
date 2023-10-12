20395ea87ac7b17f710626a2df23e069fd3bd2ca
pbellens <pbellens@gmail.com>
2023-10-12 10:00:27 +0200

    Update binary_tree.py

--- l33t/binary_tree.py
+++ l33t/binary_tree.py
@@ -8,17 +8,27 @@ class TreeNode:
         self.right = right
 
 def create_tree(nums: typing.Sequence[typing.Optional[int]], depth: int = 0, offset: int = 0) -> typing.Optional[TreeNode]:
+    print(f'offset is {offset} for depth {depth} and offset {offset}')
     if len(nums) == 0 or nums[offset] is None:
         return None
+    print(f'val {nums[offset]} jump {depth + 2 ** depth}')
     return TreeNode(
         val = nums[offset],
-        left  = create_tree(nums[2 ** depth:], depth+1, offset), 
-        right = create_tree(nums[2 ** depth:], depth+1, depth + offset + 1))
+        left  = create_tree(nums[2 ** depth:], depth+1, offset * 2), 
+        right = create_tree(nums[2 ** depth:], depth+1, offset * 2 + 1))
 
 def print_tree(root: typing.Optional[TreeNode], depth: int = 0):
+    gap = ' ' * depth
+    print(f'{gap}{root.val if root else "."}')
+    if root is not None and (root.left is not None or root.right is not None):
+        print_tree(root.left, depth+1)
+        print_tree(root.right, depth+1)
+
+def print_tree_orig(root: typing.Optional[TreeNode], depth: int = 0):
     if root is not None:
         gap = ' ' * depth
         print(f'{gap}{root.val}')
         print_tree(root.left, depth+1)
         print_tree(root.right, depth+1)
+
 
